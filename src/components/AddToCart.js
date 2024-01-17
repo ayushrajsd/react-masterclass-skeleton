142347c23b3f49ee7e5f52144c755e1be5035efb
Ayush <ayush.raj.sd@gmail.com>
2024-01-17 07:59:44 +0530

    updated add to cart and css

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -1,10 +1,34 @@
-const AddToCart = () => {
-  return (
-    <div>
-      <button className="add-to-cart" onClick="">
-        Add to Cart
-      </button>
-    </div>
-  );
+
+const AddToCart = ({ product }) => {
+
+  const productInCart = false;
+
+  const handleAdd = () => {
+  };
+  const handleRemove = () => {
+  };
+  if (!productInCart) {
+    return (
+      <div>
+        <button className="add-to-cart" onClick={handleAdd}>
+          Add to Cart
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className="add-to-cart-container">
+        <div onClick={handleRemove} className="add">
+          -
+        </div>
+        {productInCart.quantity}
+        <div className="add" onClick={handleAdd}>
+          +
+        </div>
+        ;
+      </div>
+    );
+  }
 };
+
 export default AddToCart;
